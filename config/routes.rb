cff216a1aea3dcfef41d712f4ad934c4e42d0556
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-02-28 20:07:11 -0500

    Removes incorrect home controller, adds high voltage gem, gets up homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'pages/soccer/woso_writers' => 'high_voltage/pages#show', id: 'woso_writers'
 
-  root 'home#index'
-
+  # root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
