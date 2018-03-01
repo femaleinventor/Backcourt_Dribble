e724efc240a9c262ccd14be8f965ba6185e8d9eb
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-02-28 22:57:05 -0500

    Works on layout for homepage, adds logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/soccer/woso_writers' => 'high_voltage/pages#show', id: 'woso_writers'
-
-  # root 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
