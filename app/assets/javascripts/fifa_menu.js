ea1dcf44ba594dfa4e5349685151267e211d4ab3
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-03-13 13:17:45 -0400

    Adds functionality to hide and show different lists when FIFA is clicked

--- app/assets/javascripts/fifa_menu.js
+++ app/assets/javascripts/fifa_menu.js
@@ -1,13 +1,11 @@
-$(document).on("ready", function(){
-  $(document).on("turbolinks:load", function(){
+  $(document).on("ready", function(){
+
+    // Hides FIFA Team list on load
+    $('.fifa-league-link').hide();
+
+    // When FIFA link is clicked: Hide home league list and show FIFA team list
     $('#fifa').on("click", function(){
       $('.league-link').hide();
+      $('.fifa-league-link').show();
     });
   });
-});
-
-
-// When fifa link is clicked
-  // hide all other links
-  // show new list of links for fifa Teams
-  // back navigation
