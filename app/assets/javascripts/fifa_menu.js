c88bdd87f96d23d728ab1d0c2435358af01c7922
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-03-20 20:04:16 -0400

    Updates breakpoints from sm to md

--- app/assets/javascripts/fifa_menu.js
+++ app/assets/javascripts/fifa_menu.js
@@ -6,6 +6,11 @@
     // When FIFA link is clicked: Hide home league list and show FIFA team list
     $('#fifa').on("click", function(){
       $('.league-link').hide();
+      $('.league_menu').height(2340);
       $('.fifa-league-link').show();
     });
   });
+
+
+// on click extend height of fifa menu class list to 3040px;
+// when returning to leagues shrink it to ~700px
