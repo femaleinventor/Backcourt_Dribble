ec0282b641859484401ba16a36977ea05f11f4a5
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-03-20 21:20:43 -0400

    Adds nav partial to all files and fixes js bug

--- app/assets/javascripts/fifa_menu.js
+++ app/assets/javascripts/fifa_menu.js
@@ -1,16 +1,21 @@
   $(document).on("ready", function(){
 
-    // Hides FIFA Team list on load
-    $('.fifa-league-link').hide();
+    if ($('.league-link').length > 1) {
+      // Hides FIFA Team list on load
+      $('.fifa_team_link').hide();
 
-    // When FIFA link is clicked: Hide home league list and show FIFA team list
-    $('#fifa').on("click", function(){
-      $('.league-link').hide();
-      $('.league_menu').height(2340);
+      // When FIFA link is clicked: Hide home league list and show FIFA team list
+      $('#fifa').on("click", function(){
+        $('.league-link').hide();
+        $('.league_menu').height(2340);
+        $('.fifa_team_link').show();
+      });
+
+    }
+
+    else {
       $('.fifa-league-link').show();
-    });
+      $('.league_menu').height(2340);
+    }
+
   });
-
-
-// on click extend height of fifa menu class list to 3040px;
-// when returning to leagues shrink it to ~700px
