80c997f1fc831ed25049517a47efb531093e18c2
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-03-21 18:42:15 -0400

    Increases league menu height

--- app/assets/javascripts/fifa_menu.js
+++ app/assets/javascripts/fifa_menu.js
@@ -7,7 +7,7 @@
       // When FIFA link is clicked: Hide home league list and show FIFA team list
       $('#fifa').on("click", function(){
         $('.league-link').hide();
-        $('.league_menu').height(2340);
+        $('.league_menu').height(2500);
         $('.fifa_team_link').show();
       });
 
@@ -15,7 +15,7 @@
 
     else {
       $('.fifa-league-link').show();
-      $('.league_menu').height(2340);
+      $('.league_menu').height(2500);
     }
 
   });
