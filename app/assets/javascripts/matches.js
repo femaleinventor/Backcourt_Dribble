a0823579b4c74f54d3a936faf0c64b74b6f7f439
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-05-24 19:02:55 -0400

    Did some magic.

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
 		parallaxKey: 'YmFja2NvdXJ0ZHJpYmJsZS5jb21fanh6Y0dGeVlXeHNZWGc9ZDYx',
 		parallax: true,
 		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6']
-  });
+		sectionsColor: ['#ccc', '#fff', 'blue', 'red']
+	});
 });
-
-// $(document).ready(function() {
-// 	$('#fullpage').fullpage();
-// });
