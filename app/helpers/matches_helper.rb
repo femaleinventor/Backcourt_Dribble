05cfb3045744ed9484e150380871b776894ee177
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-06-12 19:58:48 -0400

    Styles matches index slides

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -6,4 +6,9 @@ module MatchesHelper
       puts match.start
     end
   end
+
+  def format_match_date(match)
+    match_date = match.start
+    match_date.strftime("%m/%d/%Y at %I:%M%p")
+  end
 end
