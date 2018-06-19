485da7440adc9eafe06dad0d8a0e30cafa4eb685
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-06-19 18:25:25 -0400

    Re-organizes prediction and match js as well as fullpage init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,13 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		// parallaxKey: 'YmFja2NvdXJ0ZHJpYmJsZS5jb21fanh6Y0dGeVlXeHNZWGc9ZDYx',
+		// parallax: true,
+		// parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+		sectionsColor: ['black', '#707070', 'black', '#707070']
+	});
+});
