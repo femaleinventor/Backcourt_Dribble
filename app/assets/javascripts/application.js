3830c32ffbf38cc8072e000081864b1d4584bf1a
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-06-19 20:28:13 -0400

    Writes helper methods to calculate prediction percentages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,6 +27,6 @@ $(document).ready(function() {
 		// parallaxKey: 'YmFja2NvdXJ0ZHJpYmJsZS5jb21fanh6Y0dGeVlXeHNZWGc9ZDYx',
 		// parallax: true,
 		// parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-		sectionsColor: ['black', '#707070', 'black', '#707070']
+		sectionsColor: ['black', '#707070', 'black', '#707070'],
 	});
 });
--- app/helpers/predictions_helper.rb
+++ app/helpers/predictions_helper.rb
@@ -1,2 +1,20 @@
 module PredictionsHelper
+  def select_team_predictions(match, team)
+    match.predictions.select {|prediction| prediction.guess == team.name }
+  end
+
+  def calculate_total_predictions(match)
+    match.predictions.count
+  end
+
+  def calculate_prediction_percentage(match, team)
+    predictions = select_team_predictions(match, team)
+    total_team_predictions = predictions.count
+    if total_team_predictions > 0
+      percentage_of_predictions = ((total_team_predictions) / (calculate_total_predictions(match)) * 100)
+      "#{percentage_of_predictions}%"
+    else
+      "There are no predictions for this team yet!"
+    end
+  end
 end
