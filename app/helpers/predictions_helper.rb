3109e69c3d2bce73f5ce5b37f0e625313ebdfdf1
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-06-20 18:44:56 -0400

    Adds tables to leaderboard slide for each type of prediction

--- app/helpers/predictions_helper.rb
+++ app/helpers/predictions_helper.rb
@@ -3,18 +3,26 @@ module PredictionsHelper
     match.predictions.select {|prediction| prediction.guess == team.name }
   end
 
+  def select_tie_predictions(match, keyword)
+    match.predictions.select {|prediction| prediction.guess == keyword }
+  end
+
+  def build_prediction_types_array(home, team, tie)
+    array = [home, team, tie]
+  end
+
   def calculate_total_predictions(match)
     match.predictions.count
   end
 
-  def calculate_prediction_percentage(match, team)
-    predictions = select_team_predictions(match, team)
-    total_team_predictions = predictions.count
-    if total_team_predictions > 0
-      percentage_of_predictions = ((total_team_predictions) / (calculate_total_predictions(match)) * 100)
+  def calculate_prediction_percentage(match, guess)
+    predictions = select_team_predictions(match, guess)
+    total_predictions = predictions.count
+    if total_predictions > 0
+      percentage_of_predictions = ((total_predictions) / (calculate_total_predictions(match)) * 100)
       "#{percentage_of_predictions}%"
     else
-      "There are no predictions for this team yet!"
+      "There are no predictions for this outcome yet!"
     end
   end
 end
