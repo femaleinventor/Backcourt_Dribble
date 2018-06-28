64f6abbd9060f403f38b02bf32d634e5df2ffb9d
duffydduffyd <denise@foodfeedback.com>
2018-06-27 19:54:20 -0700

    Create store items, change styling in site, global grey

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,6 @@ $(document).ready(function() {
 		// parallaxKey: 'YmFja2NvdXJ0ZHJpYmJsZS5jb21fanh6Y0dGeVlXeHNZWGc9ZDYx',
 		// parallax: true,
 		// parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-		sectionsColor: ['black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070','black', '#707070', 'black', '#707070']
+		// sectionsColor: ['black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070', 'black', '#707070','black', '#707070', 'black', '#707070','black', '#707070','black', '#707070', 'black', '#707070']
 	});
 });
