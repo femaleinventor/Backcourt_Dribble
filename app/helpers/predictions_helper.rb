de8f8816b03809a0030221d688d87628383cc989
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-07-02 19:32:22 -0400

    Fixes percent prediction calculation bug

--- app/helpers/predictions_helper.rb
+++ app/helpers/predictions_helper.rb
@@ -1,28 +1,44 @@
 module PredictionsHelper
   def select_team_predictions(match, team)
-    match.predictions.select {|prediction| prediction.guess == team.name }
+    predictions = match.predictions.select {|prediction| prediction.guess == team.name }
   end
 
   def select_tie_predictions(match, keyword)
-    match.predictions.select {|prediction| prediction.guess == keyword }
+    predictions = match.predictions.select {|prediction| prediction.guess == keyword }
+    # predictions.count
   end
 
-  def build_prediction_types_array(home, team, tie)
-    array = [home, team, tie]
-  end
+  # def build_prediction_types_array(home, team, tie)
+  #   array = [home, team, tie]
+  # end
 
   def calculate_total_predictions(match)
-    match.predictions.count
+    match.predictions.count.to_f
   end
 
-  def calculate_prediction_percentage(match, guess)
+  def calculate_team_prediction_percentage(match, guess)
     predictions = select_team_predictions(match, guess)
     total_predictions = predictions.count
     if total_predictions > 0
-      percentage_of_predictions = ((total_predictions) / (calculate_total_predictions(match)) * 100)
-      "#{percentage_of_predictions}%"
+      percentage_of_predictions = ((total_predictions.to_f / calculate_total_predictions(match)) * 100).to_i
+      format_percent(percentage_of_predictions)
     else
       "There are no predictions for this outcome yet!"
     end
+  end
+
+  def calculate_tie_prediction_percentage(match, guess)
+    predictions = select_tie_predictions(match, guess)
+    total_predictions = predictions.count
+    if total_predictions > 0
+      percentage_of_predictions = ((total_predictions.to_f / calculate_total_predictions(match)) * 100).to_i
+      format_percent(percentage_of_predictions)
+    else
+      "There are no predictions for this outcome yet!"
+    end
+  end
+
+  def format_percent(num)
+    "#{num}%"
   end
 end
