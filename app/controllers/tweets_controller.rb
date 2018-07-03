49408785a47af8e086b8a1da2afce05827fc702c
Denise Duffy <denise@foodfeedback.com>
2018-07-03 09:38:43 -0700

    Change ruby version, add basic Twitter Rest client

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,15 @@ class TweetsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
+  client = Twitter::REST::Client.new do |config|
+  config.consumer_key    = "YOUR_CONSUMER_KEY"
+  config.consumer_secret = "YOUR_CONSUMER_SECRET"
+
+  client.search("to:USWNT", result_type: "recent").take(3).each do |tweet|
+    puts tweet.text
+  end
+
 end
