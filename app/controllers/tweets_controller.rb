2043782f2e39a2a6cb4ef7571f7ca57cf5a5402e
Denise Duffy <denise@foodfeedback.com>
2018-07-03 10:50:05 -0700

    Move logic to model, add some text to wall area

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,25 +6,5 @@ class TweetsController < ApplicationController
   def show
   end
 
-    # client = Twitter::REST::Client.new do |config|
-    # config.consumer_key    = "YOUR_CONSUMER_KEY"
-    # config.consumer_secret = "YOUR_CONSUMER_SECRET"
-    # end
-
-    config = {
-      consumer_key:    "YOUR_CONSUMER_KEY",
-      consumer_secret: "YOUR_CONSUMER_SECRET",
-    }
-
-    client = Twitter::REST::Client.new(config)
-
-    client.user("backcourtdribbl")
-
-    puts client.bearer_token
-
-      # client.search("to:USWNT", result_type: "recent").take(5).each
-      #   do |tweet|
-      #     puts tweet.text
-      # end
 
 end
