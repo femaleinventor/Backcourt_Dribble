04c339724b1983631e5e7ee9b4d6d1de0227ac67
Denise Duffy <denise@foodfeedback.com>
2018-07-03 10:03:40 -0700

    Change ruby version

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,12 +6,25 @@ class TweetsController < ApplicationController
   def show
   end
 
-  client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = "YOUR_CONSUMER_KEY"
-  config.consumer_secret = "YOUR_CONSUMER_SECRET"
+    # client = Twitter::REST::Client.new do |config|
+    # config.consumer_key    = "YOUR_CONSUMER_KEY"
+    # config.consumer_secret = "YOUR_CONSUMER_SECRET"
+    # end
 
-  client.search("to:USWNT", result_type: "recent").take(3).each do |tweet|
-    puts tweet.text
-  end
+    config = {
+      consumer_key:    "YOUR_CONSUMER_KEY",
+      consumer_secret: "YOUR_CONSUMER_SECRET",
+    }
+
+    client = Twitter::REST::Client.new(config)
+
+    client.user("backcourtdribbl")
+
+    puts client.bearer_token
+
+      # client.search("to:USWNT", result_type: "recent").take(5).each
+      #   do |tweet|
+      #     puts tweet.text
+      # end
 
 end
