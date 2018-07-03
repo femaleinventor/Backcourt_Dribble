4e548e8efd943e10561850ab57a5ebe09029b1fe
duffydduffyd <denise@foodfeedback.com>
2018-07-02 19:15:07 -0700

    Add config.assets.compile = true to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.assets.compile = true
 
   # Sets development default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
