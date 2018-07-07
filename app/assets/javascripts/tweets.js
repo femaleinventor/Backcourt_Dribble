b56e254909b17ddd055f47cb168923725ac65180
Denise Duffy <denise@foodfeedback.com>
2018-07-07 14:37:12 -0700

    Make basic outline for possible tweets to happen on page

---
+++ app/assets/javascripts/tweets.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	attachEventListeners();
+});
+
+
+function attachEventListeners() {
+  searchWoSo();
+  searchUSWNT();
+  searchNWSL();
+}
+
+function searchWoSo() {
+  $('.woso-link').click(function(e){
+		$('.tweets').toggle("slow");
+	});
+}
+
+function searchWoSo() {
+  $('.woso-link').click(function(e){
+		$('.tweets').toggle("slow");
+	});
+}
+
+function searchWoSo() {
+  $('.woso-link').click(function(e){
+		$('.tweets').toggle("slow");
+	});
+}
