84c5f1f89d32a254c72af191332fa5c99c96c747
Denise Duffy <denise@foodfeedback.com>
2018-07-07 19:07:21 -0700

    Adds media queries

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
       }
     tweet2 = {
       created_at: "Wed Jul 07 20:00:51 +0000 2018",
-      text: "OMG I LOVE THE SPORTSBALL GAME WITH THE LADIES",
+      text: "OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG: I LOVE THE SPORTSBALL GAME WITH THE LADIES #GoLadies #GoSportsball",
       screen_name: "denise_duffy",
       location: "SF",
       }
