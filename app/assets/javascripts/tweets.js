5077cd582edb9ab1a71eeddb22cf41b95030f1d5
Denise Duffy <denise@foodfeedback.com>
2018-07-07 18:20:43 -0700

    Add CSS and change structure of tweets wall

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -10,19 +10,42 @@ function attachEventListeners() {
 }
 
 function searchWoSo() {
-  $('.woso-link').click(function(e){
-		$('.tweets').toggle("slow");
+  $('#woso-link').on(click)
+    // on click make a call to twitter and return a hash
+    // parse the hash
+    // return parsed hash to dom
 	});
 }
 
 function searchWoSo() {
-  $('.woso-link').click(function(e){
-		$('.tweets').toggle("slow");
+  $('#uswnt-link').on(click)
+    // on click make a call to twitter and return a hash
+    // parse the hash
+    // return parsed hash to dom
 	});
 }
 
 function searchWoSo() {
-  $('.woso-link').click(function(e){
-		$('.tweets').toggle("slow");
+  $('#nwsl-link').on(click)
+    // on click make a call to twitter and return a hash
+    // parse the hash
+    // return parsed hash to dom
 	});
 }
+
+
+// make method to parse the hash
+// Parse the hash to
+// screen_name
+// location
+// text
+
+
+
+
+// Parsing best-practices
+// Twitter JSON is encoded using UTF-8 characters.
+// Parsers should tolerate variance in the ordering of fields with ease. It should be assumed that Tweet JSON is served as an unordered hash of data.
+// Parsers should tolerate the addition of 'new' fields. The Twitter platform has continually evolved since 2006, so there is a long history of new metadata being added to Tweets.
+// JSON parsers must be tolerant of ‘missing’ fields, since not all fields appear in all contexts.
+// It is generally safe to consider a nulled field, an empty set, and the absence of a field as the same thing
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,20 @@
 class TweetsController < ApplicationController
 
   def index
+    tweet = {
+      created_at: "Wed Jul 04 20:50:51 +0000 2018",
+      text: "RT @NWSL:  Happy Fourth of July from the #NWSL! https://t.co/AmuIUoBB8O",
+      screen_name: "lostinthegame_",
+      location: "Long Islander",
+      }
+    tweet2 = {
+      created_at: "Wed Jul 07 20:00:51 +0000 2018",
+      text: "OMG I LOVE THE SPORTSBALL GAME WITH THE LADIES",
+      screen_name: "denise_duffy",
+      location: "SF",
+      }
+
+    @tweets = [tweet, tweet2]
   end
 
   def show
