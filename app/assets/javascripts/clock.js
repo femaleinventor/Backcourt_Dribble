2b4d3b7f9620ba09e8a4ad7d754c7a627aa293d2
Denise Duffy <denise@foodfeedback.com>
2018-07-09 10:12:11 -0700

    Hashes out almost all javascripts except fullpage

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-  $('[data-countdown]').each(function() {
-    var $this = $(this), finalDate = $(this).data('countdown');
-    $this.countdown(finalDate, function(event) {
-      $this.html(event.strftime('%D days %H:%M:%S'));
-    });
-  });
-
-});
+// $(document).ready(function(){
+//   $('[data-countdown]').each(function() {
+//     var $this = $(this), finalDate = $(this).data('countdown');
+//     $this.countdown(finalDate, function(event) {
+//       $this.html(event.strftime('%D days %H:%M:%S'));
+//     });
+//   });
+//
+// });
