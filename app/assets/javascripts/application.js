3070b56cd332a70f141df15fa462ce8f1d1b6411
Denise Duffy <denise@foodfeedback.com>
2018-07-08 17:58:07 -0700

    Add back old fullpage javascript hashed out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,5 +23,9 @@
 
 $(document).ready(function() {
 	$('#fullpage').fullpage({
+			// parallaxKey: ‘YmFja2NvdXJ0ZHJpYmJsZS5jb21fanh6Y0dGeVlXeHNZWGc9ZDYx’,
+    	// parallax: true,
+      // parallaxOptions: {type: ‘reveal’, percentage: 62, property: ‘translate’},
+      // sectionsColor: [‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’,‘black’, ‘#707070’,‘black’, ‘#707070’, ‘black’, ‘#707070’]
 	});
 });
