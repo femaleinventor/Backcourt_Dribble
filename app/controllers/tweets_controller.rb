ec2bd84eb3c627be405dbe6564a21d47386deadf
Denise Duffy <denise@foodfeedback.com>
2018-07-09 12:27:19 -0700

    Rehash in controller tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,13 +4,13 @@ class TweetsController < ApplicationController
     tweet = {
       created_at: "Wed Jul 04 20:50:51 +0000 2018",
       text: "RT @NWSL:  Happy Fourth of July from the #NWSL! https://t.co/AmuIUoBB8O",
-      # screen_name: "lostinthegame_",
+      screen_name: "lostinthegame_",
       location: "Long Islander",
       }
     tweet2 = {
       created_at: "Wed Jul 07 20:00:51 +0000 2018",
       text: "OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG OMG: I LOVE THE SPORTSBALL GAME WITH THE LADIES #GoLadies #GoSportsball",
-      # screen_name: "denise_duffy",
+      screen_name: "denise_duffy",
       location: "SF",
       }
 
