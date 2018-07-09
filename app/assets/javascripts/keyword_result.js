51846e6c604f9d7be60446d47164862ba22df3d3
Denise Duffy <denise@foodfeedback.com>
2018-07-09 11:12:51 -0700

    Hash out screen name from tweets controller, keyword_result, tweets index view

--- app/assets/javascripts/keyword_result.js
+++ app/assets/javascripts/keyword_result.js
@@ -1,21 +1,21 @@
-new_tweet = {
-  created_at:
-  screen_name:
-  location:
-  text:
-}
-
-
-  new_tweet = {
-      "created_at": "Wed Jul 04 20:50:51 +0000 2018",
-      "text": "RT @NWSL: 🎇 Happy Fourth of July from the #NWSL! 🎇 https://t.co/AmuIUoBB8O",
-      "user": {
-        "screen_name": "lostinthegame_",
-        "location": "Long Islander",
-      },
-    }
-
-t.created_at
-t.text
-t.user.screen_name
-t.user.location
+// new_tweet = {
+//   created_at:
+//   screen_name:
+//   location:
+//   text:
+// }
+//
+//
+//   new_tweet = {
+//       "created_at": "Wed Jul 04 20:50:51 +0000 2018",
+//       "text": "RT @NWSL: 🎇 Happy Fourth of July from the #NWSL! 🎇 https://t.co/AmuIUoBB8O",
+//       "user": {
+//         "screen_name": "lostinthegame_",
+//         "location": "Long Islander",
+//       },
+//     }
+//
+// t.created_at
+// t.text
+// t.user.screen_name
+// t.user.location
