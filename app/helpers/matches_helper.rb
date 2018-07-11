526befca37332b2d6ef22ab43dceac9b1b65064b
[01;31m[K"name"[m[K: "Ethan Fertsch <[01;31m[K"email"[m[K: "efertsch@gmail.com>
2018-07-11 18:51:44 -0400

    Styles venue information page based on wireframe

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -9,6 +9,17 @@ module MatchesHelper
     match_date.strftime("%m/%d/%Y at %I:%M%p")
   end
 
+  def format_match_date_english(match)
+     date = match.start.to_date.strftime("%B #{match.start.to_date.day.ordinalize}, %Y")
+     time = match.start.strftime("%I:%M%p")
+     "#{date} at #{time}"
+  end
+
+  # def get_local_zone(match)
+  #   match_date = match.start
+  #   match_date.in_time_zone
+  # end
+
   def sort_matches_by_date(matches)
     matches.sort_by{ |match| match.start }
   end
