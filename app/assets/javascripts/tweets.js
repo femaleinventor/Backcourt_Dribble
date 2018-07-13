e08ccf94cd65df1ba259f3e36ad9d6998b008cb6
duffydduffyd <denise@foodfeedback.com>
2018-07-12 19:13:53 -0700

    JQuery attempt (broken)

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,48 +1,62 @@
-// $(document).ready(function() {
-// 	attachEventListeners();
-// });
+$(document).ready(function() {
+	attachEventListeners();
+});
 
 
-// function attachEventListeners() {
-//   searchWoSo();
-//   searchUSWNT();
-//   searchNWSL();
+function attachEventListeners() {
+  $('.keyword').on('click', function(e) {
+    console.log("You click'n shit.");
+    e.preventDefault();
+    // console.log(this)
+    // console.log($(this))
+    var keyword = $(this).attr('id');
+    console.log(keyword)
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: { keyword: keyword},
+    }).done(function(res){
+      console.log(res);
+      // $(e.target).replaceWith(res);
+    })
+  });
+}
+
+// function keywordClick() {
+//   console.log("You click'n shit.");
+//   e.preventDefault();
+//   let keyword = $(this).data("id");
+//   $.ajax({
+//     url: '/tweets/index',
+//     method: 'GET',
+//     data: { keyword: keyword }
+//   }).done(function(res){
+//     console.log(res);
+//     // $(e.target).replaceWith(res);
+//   })
 // }
 
+// click on a keyword
+// fetch tweets for keyword
+// clear tweet feed.
+// for each tweet,
+//   move previous tweet up
+//   add new tweet to bottom
+// end
+// fetch more tweets!
+
+
 // function searchWoSo() {
-//     // on click make a call to twitter and return a hash
-//     // parse the hash
-//     // return parsed hash to dom
+
 // 	});
 // }
 
 // function searchWoSo() {
-//     // on click make a call to twitter and return a hash
-//     // parse the hash
-//     // return parsed hash to dom
+
 // 	});
 // }
 
 // function searchWoSo() {
-//     // on click make a call to twitter and return a hash
-//     // parse the hash
-//     // return parsed hash to dom
+
 // 	});
 // }
-
-
-// make method to parse the hash
-// Parse the hash to
-// screen_name
-// location
-// text
-
-
-
-
-// Parsing best-practices
-// Twitter JSON is encoded using UTF-8 characters.
-// Parsers should tolerate variance in the ordering of fields with ease. It should be assumed that Tweet JSON is served as an unordered hash of data.
-// Parsers should tolerate the addition of 'new' fields. The Twitter platform has continually evolved since 2006, so there is a long history of new metadata being added to Tweets.
-// JSON parsers must be tolerant of ‘missing’ fields, since not all fields appear in all contexts.
-// It is generally safe to consider a nulled field, an empty set, and the absence of a field as the same thing
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,12 +2,19 @@
 class TweetsController < ApplicationController
 
   def index
+    p params
     @tweets = params[:keyword].nil? ? [] : TwitterAdapter::search(params[:keyword])
   end
 
   def create
-    @search_string = params[:keyword]
-    @tweets = TwitterAdapter::search(@search_string)
+    p "You're creating"
+    p params
+    @tweets = params[:keyword].nil? ? [] : TwitterAdapter::search(params[:keyword])
+    if request.xhr?
+      p "OMG ITS A JSON REQUEST WHOOOOOOOOOOOOOO!"
+      @tweets.to_json
+    end
+    render :_tweet, layout: false
   end
 
   def show
