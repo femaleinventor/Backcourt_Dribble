129f812d3d2b2492e761aee07ee37c06e5ad0619
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-08-14 20:41:02 -0400

    Adds forms to update and delete sports

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -35,8 +35,30 @@ class SportsController < ApplicationController
     @sport = Sport.find(params[:id])
   end
 
+  def update
+    @sport = Sport.find(params[:id])
+    if @sport.update(update_sport_params)
+        redirect_to sport_path(@sport), alert: "Sport updated successfully!"
+    else
+        redirect_to sport_path(@sport), alert: "Oops! Your update to #{@sport.name} wasn't recorded. Try again!"
+    end
+  end
+
+  def destroy
+    @sport = Sport.find(params[:id])
+    if @sport.destroy
+      redirect_to admin_index_path
+    else
+      redirect_to sport_path(@sport), alert: "Oops! #{@sport.name} wasn't deleted. Try again!"
+    end
+  end
+
   private
     def sport_params
       params.permit(:name, :image_url)
+    end
+
+    def update_sport_params
+      params.require(:sport).permit(:name, :image_url)
     end
 end
