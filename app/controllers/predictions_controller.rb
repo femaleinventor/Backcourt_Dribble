00df2fdb26dec0ec43fa9c485109a4ef1f27ec8d
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-08-15 20:27:41 -0400

    Adds validations on model

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -6,13 +6,13 @@ class PredictionsController < ApplicationController
 
   def create
     @prediction = Prediction.new(prediction_params)
+    @league = @prediction.match.league
     if @prediction.save
       # Returns to matches#index page after prediction submission
-      @league = @prediction.match.league
       redirect_to league_matches_path(@league), alert: "Thanks for making a prediction!"
       # redirect_to "/", alert: "Thanks for making a prediction!"
     else
-        redirect_to league_matches_path, alert: "Oops! Your prediction wasn't recorded. Try again!"
+        redirect_to league_matches_path(@league), alert: "Oops! Your prediction wasn't recorded. Try again!"
     end
   end
 
