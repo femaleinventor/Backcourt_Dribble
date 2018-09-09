dd142bd9b3fe650dbca391aef6cf505028ede97d
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-09-09 17:09:26 -0400

    changes admin venues create form_tag to form_for

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -39,10 +39,10 @@ class VenuesController < ApplicationController
   private
     def venue_params
       # params.require(:venue).permit(:name, :image_url, :address, :time_zone)
-      params.require(:venue).permit(:name, :image_url, :address, :time_zone)
+      params.require(:venue).permit(:name, :picture_url, :address, :time_zone)
     end
 
     def update_venue_params
-      params.require(:venue).permit(:name, :image_url, :address, :time_zone)
+      params.require(:venue).permit(:name, :picture_url, :address, :time_zone)
     end
 end
