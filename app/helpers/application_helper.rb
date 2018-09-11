d0064ff48748579887380f014c130823c21a543d
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-09-11 18:11:04 -0400

    Adds google analytics methods to application helper and adds tags to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
     users.sort_by{|u| u.points}.reverse
   end
 
+  def ga_script
+    tracking_id = "UA-125640699-1"
+    if Rails.env.production?
+      javascript_tag("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '#{tracking_id}', 'auto');")
+    end
+  end
+
+  def ga_track
+    javascript_tag("if(window.ga != undefined){ga('send', 'pageview');}")
+  end
+
 end
