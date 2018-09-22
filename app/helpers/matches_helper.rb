911934f6a6064570efe464a28e8ff58d0c924a82
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-09-22 11:03:56 -0400

    Adds helper methods to convert to local time

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -9,8 +9,26 @@ module MatchesHelper
     matches.sort_by{|match| match.start }
   end
 
+  def convert_to_local_time(match)
+    match.start.to_time.localtime
+  end
+
   def format_match_date(match)
     match.start.strftime("%m/%d/%Y at %I:%M%p")
+  end
+
+  def get_offset_of_venue(match)
+    seconds = ActiveSupport::TimeZone[match.venue.time_zone].utc_offset
+    seconds / 60 / 60
+  end
+
+  def add_offset_to_match_start(match)
+    match.start.change(:offset => get_offset_of_venue(match))
+  end
+
+  def format_local_time(match)
+    local_time = convert_to_local_time(match)
+    local_time.strftime("%m/%d/%Y at %I:%M%p")
   end
 
   def format_match_date_english(match)
@@ -38,6 +56,16 @@ module MatchesHelper
   def insert_spaces(string)
     !check_for_spaces(string) ? string.scan(/.{50}|.+/).join(" ") : string
   end
+
+
+  # When creating a new match
+    #Update db and admin forms to:
+      # Add TimeZone dropdown to Match
+      # Remove from Venue
+      # Get offset from TimeZone dropdown, based on location AND date of match
+
+
+
 
 
 end
