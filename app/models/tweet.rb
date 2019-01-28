01559fb1f516390a0bc7c489d7e3559a62323382
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2019-01-28 18:03:56 -0500

    Adds associations for quoted tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :twitter_user
   has_and_belongs_to_many :hashtags
+  has_many :quoted_tweets, :class_name => "Tweet",
+    :foreign_key => "quoted_tweet_id"
+  belongs_to :quoted_tweet, :class_name => "Tweet",
+    :foreign_key => "quoted_tweet_id", optional: true
 end
