746dcd271645a0d9061c7485524da4bcc5b10cd2
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2019-01-29 13:22:56 -0500

    Changes polling interval for sidekiq cron job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
-schedule_file = "config/schedule.yml"
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+Sidekiq.configure_server do |config|
+  schedule_file = "config/schedule.yml"
+
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
+
+  Sidekiq.options[:poll_interval] = 3
 end
