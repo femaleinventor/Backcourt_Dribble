f7c6eb44ef5f26a3fff80f3234ba5d8786a10e75
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2019-01-29 16:56:17 -0500

    Reroutes root to tweets#index

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,6 @@ Rails.application.routes.draw do
   get 'pages/soccer/woso_writers' => 'high_voltage/pages#show', id: 'woso_writers'
 
 
-  root to: "home#index"
+  root to: "tweets#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
